7a3d33629947483eed7e77dacf1bc4c010d2316c
Sajid Karim <56113530+sajid-karim@users.noreply.github.com>
2022-06-16 19:17:29 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,56 @@
+const express = require('express');
+const ZKLib = require('node-zklib');
+const db = require('./common/connectdb');
+
+
+const app = express();
+
+// PORT
+const PORT = 3000;
+
+    // connect the biometric machine
+const ip = `10.1.0.160`;
+const port = 4370;
+const timeout = 10000;
+
+let zkInstance = new ZKLib(ip,port,timeout,4000);
+
+
+// routes
+
+// get users
+app.get(`/users`,async(request, response)=>{
+
+    try {
+        // Create socket to machine 
+        await zkInstance.createSocket()
+
+        // get users from the machine
+        response.send(await zkInstance.getUsers());
+    } catch(e){
+        response.sendStatus(401);
+    }    
+});
+
+// get attendance records
+app.get(`/attendances`, async(request, response)=>{
+    try {
+        // Create socket to machine 
+        await zkInstance.createSocket()
+
+        // get users from the machine
+        const logs = await zkInstance.getAttendances()
+        // console.log(typeof(logs));
+        let attendancesData = logs.data;
+
+        response.send(attendancesData);
+    } catch(e){
+        response.sendStatus(401);
+    }
+
+});
+
+app.listen(PORT, () => {
+   console.log(`Server is running on PORT: ${PORT}`);
+});
+
